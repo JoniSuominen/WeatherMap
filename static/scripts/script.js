c5e38047d97d62c18b034da49f76f5941e89c065
Joni Suominen <joni.suominen98@gmail.com>
2018-09-22 14:23:45 +0300

    change http to https

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -80,7 +80,7 @@ function clearBubbles() {
 
 
 function getWeather(parameters, callback) {
-$.getJSON("http://0.0.0.0:5000/weather", parameters, function(weather, textStatus, jqXHR) {
+$.getJSON("https://0.0.0.0:5000/weather", parameters, function(weather, textStatus, jqXHR) {
     callback(weather);
 })
 
