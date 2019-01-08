af13adf7422ed21c486569d8b4d5410eef741482
Joni Suominen <joni.suominen98@gmail.com>
2019-01-08 17:06:33 +0200

    refractoring, commenting

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,18 +1,15 @@
-var AUTOCOMPLETION_URL = 'https://autocomplete.geocoder.api.here.com/6.2/suggest.json',
+let AUTOCOMPLETION_URL = 'https://autocomplete.geocoder.api.here.com/6.2/suggest.json',
 ajaxRequest = new XMLHttpRequest(),
 query = '';
 
-var APP_ID = 'gavPqgol9yAUUGHAWL0d';
-var APP_CODE = 'KwDM1mSbmqqKQ5b30YldpQ';
-var map;
-var group;
-var ui;
-var bubble;
-
-console.log("lol");
+let APP_ID = 'gavPqgol9yAUUGHAWL0d';
+let APP_CODE = 'KwDM1mSbmqqKQ5b30YldpQ';
+let map;
+let group;
+let ui;
+let bubble;
 
 // configures the typeahead-plugin for autocomplete
-
 function configure() {
 $("#q").typeahead({
   highlight: false,
@@ -32,10 +29,12 @@ $("#q").typeahead({
   }
 });
 
+// when the user selects a location from the dropdown menu
 $("#q").on("typeahead:selected", function(eventObject, suggestion, name) {
-    var geocodingParams = {
+    let geocodingParams = {
       searchText: suggestion.label
     }
+    
     geocoder.geocode(geocodingParams, onResult, function(e) {
       alert(e);
     });
@@ -45,18 +44,18 @@ $("#q").on("typeahead:selected", function(eventObject, suggestion, name) {
 }
 
 // invocated upon user clicking a search result
-var onResult = function(result) {
+let onResult = function(result) {
   clearBubbles();
 
-  var locations = result.Response.View[0].Result,
-  position, marker;
-  var loc1 = locations[0];
+  let locations = result.Response.View[0].Result
+  let loc1 = locations[0];
 
   let parameters = {
     lat: loc1.Location.DisplayPosition.Latitude,
     lon: loc1.Location.DisplayPosition.Longitude
   };
 
+  // centers the map
   map.setCenter({
     lat: parameters.lat,
     lng: parameters.lon
@@ -80,7 +79,7 @@ function clearBubbles() {
   }
 }
 
-
+// queries the back-end for weather data
 function getWeather(parameters, callback) {
 $.getJSON("/weather", parameters, function(weather, textStatus, jqXHR) {
     callback(weather);
@@ -88,17 +87,20 @@ $.getJSON("/weather", parameters, function(weather, textStatus, jqXHR) {
 
 }
 
+// adds a marker
 function addMarkerToGroup(group, coordinate, html) {
-var marker = new H.map.Marker(coordinate);
+let marker = new H.map.Marker(coordinate);
 // add custom data to the marker
 marker.setData(html);
 group.addObject(marker);
 }
 
+// configure the info bubble to show the locations' weather data
 function addInfoBubble(loc1, weather) {
 
 map.addObject(group);
 
+// open the info bubble upon clicking it
 group.addEventListener('tap', function(evt) {
   bubble = new H.ui.InfoBubble(evt.target.getPosition(), {
     content: evt.target.getData()
@@ -110,11 +112,11 @@ group.addEventListener('tap', function(evt) {
 
 
 
-var context = loc1.Location.Address.Label;
-var temp =  weather.list[0].main.temp;
-var code = weather.list[0].weather[0].id;
-var icon = '<i class ="wi wi-owm-' + code + '"></i>';
-var html = "<div class='row'> <div class='column'>" +  icon + "</div>" + "<div class=' column'><p class='infotext'> " + context + ", " + temp  +  "C" + " </p> </div>" + "</div>";
+let context = loc1.Location.Address.Label;
+let temp =  Math.round(weather.list[0].main.temp);
+let code = weather.list[0].weather[0].id;
+let icon = '<i class ="wi wi-owm-' + code + '"></i>';
+let html = "<div class='row'> <div class='column'>" +  icon + "</div>" + "<div class=' column'><p class='infotext'> " + context + ", " + temp  +  "C" + " </p> </div>" + "</div>";
 
 addMarkerToGroup(group, {lat: loc1.Location.DisplayPosition.Latitude,
 lng :loc1.Location.DisplayPosition.Longitude},
@@ -122,6 +124,7 @@ html
 );
 }
 
+// queries Here Maps API for cities based on user input thus far
 function autoCompleteSuggestions(query, syncResults, asyncResults) {
     let parameters = {
       query: encodeURIComponent(query),
@@ -136,18 +139,18 @@ function autoCompleteSuggestions(query, syncResults, asyncResults) {
 }
 
 
-var platform = new H.service.Platform({
+let platform = new H.service.Platform({
   useCIT: true,
 'app_id': APP_ID,
 'app_code': APP_CODE,
 useHTTPS : true
 });
 
-var geocoder = platform.getGeocodingService();
+let geocoder = platform.getGeocodingService();
 
 $(document).ready(function() {
 // Obtain the default map types from the platform object:
-var defaultLayers = platform.createDefaultLayers();
+let defaultLayers = platform.createDefaultLayers();
 
 
 // Instantiate (and display) a map object:
@@ -160,7 +163,7 @@ map = new H.Map(
   });
   group = new H.map.Group();
 
-  var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
+  let behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
   ui = H.ui.UI.createDefault(map, defaultLayers);
   map.addObject(group);
 
